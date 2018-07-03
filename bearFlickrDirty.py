42dd8c0689597ba165965c03c05b917ee9f8add7
Evan Burnette <EvanBurnette@gmail.com>
2018-07-02 20:27:17 -0700

    Removed unused code

--- bearFlickrDirty.py
+++ bearFlickrDirty.py
@@ -6,9 +6,6 @@ import requests
 import sys
 import codecs
 
-sys.stdout = codecs.getwriter('utf8')(sys.stdout)
-#sys.stderr = codecs.getwriter('utf8')(sys.stderr)
-
 os.chdir('/home/evan/Downloads/MyPrograms/')
 newDir = 'bear'
 if os.path.isdir(newDir) == False:
@@ -20,9 +17,12 @@ page = requests.get('http://www.flickr.com/search/?text=bear').text
 #find all jpg links
 jpgRe = re.compile('''c1.staticflickr.com.*jpg''', re.UNICODE)
 linkList = jpgRe.findall(page, re.UNICODE)
+print(len(linkList))
+for item in linkList:
+    print(item)
 
 for link in linkList:
-    image = requests.get('http://' + link)
-    imgFile = open(os.path.basename(link), 'wb')
-    for chunk in image.iter_content(100000):
-        imgFile.write(chunk)
+    image = requests.get('http:' + link)
+    imgfile = open(os.basename(link), 'wb')
+    for chunk in image.iter_contents(100000):
+        file.write(chunk)
